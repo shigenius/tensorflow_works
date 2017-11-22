1da340aefc0fef8379c5a5da6f5d5c3a093c1376
shigenius <k178578@ie.u-ryukyu.ac.jp>
2017-11-23 00:11:19 +0900

    modify train.py

--- train.py
+++ train.py
@@ -6,31 +6,12 @@ import numpy as np
 import tensorflow as tf
 import tensorflow.python.platform
 import argparse
+import random
 
+# めんば変数にする
 NUM_CLASSES = 10
 IMAGE_SIZE = 227
 IMAGE_PIXELS = IMAGE_SIZE*IMAGE_SIZE*3
-
-# flags = tf.app.flags
-# FLAGS = flags.FLAGS
-# flags.DEFINE_string('train', 'train.txt', 'File name of train data')
-# flags.DEFINE_string('test', 'test.txt', 'File name of train data')
-# flags.DEFINE_string('train_dir', '/tmp/data', 'Directory to put the training data.')
-# flags.DEFINE_integer('max_steps', 200, 'Number of steps to run trainer.')
-# flags.DEFINE_integer('batch_size', 10, 'Batch size'
-#                      'Must divide evenly into the dataset sizes.')
-# flags.DEFINE_float('learning_rate', 1e-4, 'Initial learning rate.')
-# 
-
-parser = argparse.ArgumentParser(description='Learning original dataset')
-parser.add_argument('train', help='File name of train data')
-parser.add_argument('test', help='File name of train data')
-parser.add_argument('--train_dir', '-dir', default='/tmp/data',
-                        help='Directory to put the training data.')
-parser.add_argument('--max_steps', '-s', type=int, default=100)
-parser.add_argument('--batch_size', '-batch', type=float, default=5)
-parser.add_argument('--learning_rate', '-lr', type=float, default=1e-4)
-args = parser.parse_args()
 
 def inference(images_placeholder, keep_prob):
     """ 予測モデルを作成する関数
@@ -52,17 +33,14 @@ def inference(images_placeholder, keep_prob):
       initial = tf.constant(0.1, shape=shape)
       return tf.Variable(initial)
 
-    # 畳み込み層の作成
-    def conv2d(x, W):
-      return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
     # プーリング層の作成
     def max_pool_2x2(x):
       return tf.nn.max_pool(x, ksize=[1, 3, 3, 1],
                             strides=[1, 2, 2, 1], padding='VALID')
     
-    # 入力を256x256x3に変形
+    # 入力を226x227x3に変形
     x_image = tf.reshape(images_placeholder, [-1, IMAGE_SIZE, IMAGE_SIZE, 3])
+    print("input: ", x_image.shape)
 
     # 畳み込み層1の作成
     with tf.name_scope('conv1') as scope:
@@ -92,16 +70,15 @@ def inference(images_placeholder, keep_prob):
         h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
         # dropoutの設定
         h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
+        print("h_fc1:", h_fc1.shape)
     # 全結合層2の作成
     with tf.name_scope('fc2') as scope:
         W_fc2 = weight_variable([128, NUM_CLASSES])
         b_fc2 = bias_variable([NUM_CLASSES])
-
     # ソフトマックス関数による正規化
     with tf.name_scope('softmax') as scope:
         y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
-
+        print("y :", y_conv.shape)
     # 各ラベルの確率のようなものを返す
     return y_conv
 
@@ -155,61 +132,125 @@ def accuracy(logits, labels):
     tf.summary.scalar("accuracy", accuracy)
     return accuracy
 
+class Dataset:
+
+    def __init__(self, train, test, num_classes):
+        """
+        読み込むtxtファイルは，
+        <image1_path> class
+        <image2_path> class
+        みたいに記述しておく．
+        """
+        # メンバ変数
+        self.train_image_paths = []
+        self.train_labels = [] # [[0,0,0,1,0,0,0,0,0],...] みたいな感じ (1-of-k)
+        self.test_image_paths = []
+        self.test_labels = []
+
+        # パスとラベルを取得する
+        with open(train, 'r') as f:
+            f_ = [line.rstrip().split() for line in f]
+            self.train_image_paths = [l[0] for l in f_]
+
+            for l in f_:
+                tmp = [0]*num_classes
+                tmp[int(l[1])] = 1
+                self.train_labels.append(tmp) 
+
+        self.train_labels = np.asarray(self.train_labels) #numpyにしておく
+        
+        with open(test, 'r') as f:
+            f_ = [line.rstrip().split() for line in f]
+            self.test_image_paths = [l[0] for l in f_]
+
+            for l in f_:
+                tmp = [0]*num_classes
+                tmp[int(l[1])] = 1
+                self.test_labels.append(tmp) 
+
+        self.test_labels = np.asarray(self.test_labels) 
+
+    def shuffle(self):
+        # shuffle (dataとlabelの対応が崩れないように)
+
+        lst = [i for i in range(len(self.train_image_paths))]
+        shuffled_lst = list(lst)
+        random.shuffle(shuffled_lst)
+
+        shuffled_data = self.train_image_paths
+        shuffled_labels = self.train_labels
+
+        for i, (train, label) in enumerate(zip(self.train_image_paths, self.train_labels)):
+            shuffled_data[shuffled_lst[i]] = train
+            shuffled_labels[shuffled_lst[i]] = label
+
+        self.train_image_paths = shuffled_data
+        self.train_labels = shuffled_labels
+
+        # 対応関係が破壊されてないかの確認用
+        #for i, (train, label) in enumerate(zip(self.train_image_paths, self.train_labels)):
+        #    print(i, train, label)
+
+
+    def getTrainBatch(self, batchsize, index, image_size):
+        """
+        指定したindexからバッチサイズ分，データセットを読み込んでflattenなndarrayとして返す．(resizeもする．あとでaugumentationも実装したい)
+        """
+
+        train_batch = []
+        start = batchsize*index
+
+        for path in self.train_image_paths[start:start+batchsize]:
+            image = cv2.imread(path)
+            image = cv2.resize(image, (image_size, image_size)) 
+
+            # 一列にした後、0-1のfloat値にする
+            train_batch.append(image.flatten().astype(np.float32)/255.0)
+
+        train_batch = np.asarray(train_batch)
+        labels_batch = self.train_labels[start:start+batchsize]
+
+        return train_batch, labels_batch
+        
+
+    def getTestData():
+        pass
+
 def main():
+    parser = argparse.ArgumentParser(description='Learning your dataset, and evaluate the trained model')
+    parser.add_argument('train', help='File name of train data')
+    parser.add_argument('test', help='File name of train data')
+    parser.add_argument('--train_dir', '-dir', default='/tmp/data',
+                        help='Directory to put the training data. (TensorBoard)')
+    parser.add_argument('--save_path', '-save', default='/home/akalab/tensorflow_works/model.ckpt',
+                        help='FullPath of output model')
+    parser.add_argument('--max_steps', '-s', type=int, default=100)
+    parser.add_argument('--batch_size', '-b', type=int, default=10)
+    parser.add_argument('--learning_rate', '-lr', type=float, default=1e-4)
+    parser.add_argument('--dropout_prob', '-d', type=float, default=0.5)
+    args = parser.parse_args()
 
-    # ファイルを開く
-    f = open(args.train, 'r')
-    # データを入れる配列
-    train_image = []
-    train_label = []
-    for line in f:
-        # 改行を除いてスペース区切りにする
-        line = line.rstrip()
-        l = line.split()
-        # データを読み込んで256x256に縮小
-        img = cv2.imread(l[0])
-        img = cv2.resize(img, (IMAGE_SIZE, IMAGE_SIZE))
-        # 一列にした後、0-1のfloat値にする
-        train_image.append(img.flatten().astype(np.float32)/255.0)
-        # ラベルを1-of-k方式で用意する
-        tmp = np.zeros(NUM_CLASSES)
-        tmp[int(l[1])] = 1
-        train_label.append(tmp)
-    # numpy形式に変換
-    train_image = np.asarray(train_image)
-    train_label = np.asarray(train_label) 
-    f.close()
+    num_classes = 10
+    image_size = 227
 
-    f = open(args.test, 'r')
-    test_image = []
-    test_label = []
-    for line in f:
-        line = line.rstrip()
-        l = line.split()
-        img = cv2.imread(l[0])
-        img = cv2.resize(img, (IMAGE_SIZE, IMAGE_SIZE))
-        test_image.append(img.flatten().astype(np.float32)/255.0)
-        tmp = np.zeros(NUM_CLASSES)
-        tmp[int(l[1])] = 1
-        test_label.append(tmp)
-    test_image = np.asarray(test_image)
-    test_label = np.asarray(test_label)
-    f.close()
+    #データセットの準備
+    dataset = Dataset(args.train, args.test, num_classes)
+    #dataset.shuffle()
+
     with tf.Graph().as_default():
-        # 画像を入れる仮のTensor
-        images_placeholder = tf.placeholder(dtype="float", shape=(None, IMAGE_PIXELS)) # shapeの第一引数をNoneにすることによっておそらく可変長batchに対応できる
-        # ラベルを入れる仮のTensor
-        labels_placeholder = tf.placeholder(dtype="float", shape=(None, NUM_CLASSES))
+        # imageとlabelを入れる仮のTensor
+        images_placeholder = tf.placeholder(dtype="float", shape=(None, image_size*image_size*3)) # shapeの第一引数をNoneにすることによっておそらく可変長batchに対応できる
+        labels_placeholder = tf.placeholder(dtype="float", shape=(None, num_classes))
         # dropout率を入れる仮のTensor
         keep_prob = tf.placeholder(dtype="float")
 
-        # inference()を呼び出してモデルを作る
+        # モデルを作る
         logits = inference(images_placeholder, keep_prob)
-        # loss()を呼び出して損失を計算
+        # lossを計算
         loss_value = loss(logits, labels_placeholder)
-        # training()を呼び出して訓練
+        # 訓練のoperation
         train_op = training(loss_value, args.learning_rate)
-        # 精度の計算
+        # accyracyの計算
         acc = accuracy(logits, labels_placeholder)
 
         # 保存の準備
@@ -217,48 +258,59 @@ def main():
         # Sessionの作成
         sess = tf.Session()
         # 変数の初期化
-        #sess.run(tf.initialize_all_variables())
         sess.run(tf.global_variables_initializer())
         # TensorBoardで表示する値の設定
         summary_op = tf.summary.merge_all()
         summary_writer = tf.summary.FileWriter(args.train_dir, sess.graph_def)
+
         # 訓練の実行
         for step in range(args.max_steps):
-            for i in range(int(len(train_image)/args.batch_size)):
-                # batch_size分の画像に対して訓練の実行
-                batch = args.batch_size*i
-                # feed_dictでplaceholderに入れるデータを指定する
-                #print(train_image[batch:batch+args.batch_size].shape)
-                #print(train_label[batch:batch+args.batch_size].shape)
-                # print("session run:", i)
-                sess.run(train_op, feed_dict={
-                  images_placeholder: train_image[batch:batch+args.batch_size],
-                  labels_placeholder: train_label[batch:batch+args.batch_size],
-                  keep_prob: 0.5})
 
-            # 1 step終わるたびに精度を計算する
-            
+            dataset.shuffle() # バッチで取る前にデータセットをshuffleする   
+
+            for i in range(int(len(dataset.train_image_paths)/args.batch_size)): # iがbatchのindexになる
+                batch, labels = dataset.getTrainBatch(args.batch_size, i, image_size)
+
+                # feed_dictでplaceholderに入れるデータを指定する
+                sess.run(train_op, feed_dict={
+                  images_placeholder: batch,
+                  labels_placeholder: labels,
+                  keep_prob: args.dropout_prob})
+
+                # とりあえずのbatch accuracy．あとで消す
+                train_accuracy = sess.run(acc, feed_dict={
+                images_placeholder: batch,
+                labels_placeholder: labels,
+                keep_prob: 1.0})
+                print("step %d, batch %d,  training batch accuracy %g"%(step, i, train_accuracy))
+
+
+            """ *保留* step毎にtrain accyracyをどうにか計算したいけど，全データセットをインスタンス化するのは無理．案1 : 最後のbatchのaccuracyをtrain accuracyとするみたいな．step毎にdataset.shuffle()すれば偏りはなくなるはず
+            # 1step終わるたびにaccuracyを計算する
             train_accuracy = sess.run(acc, feed_dict={
-                images_placeholder: train_image,
-                labels_placeholder: train_label,
+                images_placeholder: dataset.train_image,
+                labels_placeholder: dataset.train_label,
                 keep_prob: 1.0})
             print("step %d, training accuracy %g"%(step, train_accuracy))
 
-            # 1 step終わるたびにTensorBoardに表示する値を追加する
+            # 1step終わるたびにTensorBoardに表示する値を追加する
             summary_str = sess.run(summary_op, feed_dict={
                 images_placeholder: train_image,
                 labels_placeholder: train_label,
                 keep_prob: 1.0})
             summary_writer.add_summary(summary_str, step)
-
-    # 訓練が終了したらテストデータに対する精度を表示
+            """
+    """
+    # testdataのaccuracy
     print("test accuracy %g" % sess.run(acc, feed_dict={
         images_placeholder: test_image,
         labels_placeholder: test_label,
         keep_prob: 1.0}))
-
+    """
     # 最終的なモデルを保存
-    save_path = saver.save(sess, "/home/akalab/tensorflow_works/model.ckpt")
+    save_path = saver.save(sess, args.save_path)
     print("save the trained model at :", save_path)
+
+
 if __name__ == '__main__':
     main()
