6d60b2dcc7840863881cc3716f01530c9be512e3
shigenius <k178578@ie.u-ryukyu.ac.jp>
2018-05-21 18:21:05 +0900

    modify tf-slim/transfer_learning.py

--- tf-slim/transfer_learning.py
+++ tf-slim/transfer_learning.py
@@ -22,7 +22,7 @@ def shigeNet_v1(cropped_images, original_images, num_classes, keep_prob=1.0, is_
             with slim.arg_scope(inception_v4_arg_scope()):
                 logits_c, end_points_c = inception_v4(cropped_images, num_classes=1001, is_training=False, reuse=None)
                 # logits_o, end_points_o = inception_v4(original_images, num_classes=1001, is_training=False, reuse=tf.AUTO_REUSE)
-                logits_o, end_points_o = inception_v4(original_images, num_classes=1001, is_training=False, reuse=None)
+                logits_o, end_points_o = inception_v4(original_images, num_classes=1001, is_training=False, reuse=tf.AUTO_REUSE)
 
                 feature_c = end_points_c['PreLogitsFlatten']
                 feature_o = end_points_o['PreLogitsFlatten']
