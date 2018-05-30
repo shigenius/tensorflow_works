47750b8cd52602027583828af6e1d479ce833d6b
shigenius <k178578@ie.u-ryukyu.ac.jp>
2018-05-31 02:57:45 +0900

    modify tf-slim/trainfer_learning(visualise)

--- tf-slim/TwoInputDataset.py
+++ tf-slim/TwoInputDataset.py
@@ -122,8 +122,11 @@ class TwoInputDataset():
             imageA = cv2.resize(imageA, (self.image_size, self.image_size))
             imageB = cv2.resize(imageB, (self.image_size, self.image_size))
 
-            batchA.append(imageA.astype(np.float32)/255.0)
-            batchB.append(imageB.astype(np.float32)/255.0)
+            # batchA.append(imageA.astype(np.float32)/255.0)
+            # batchB.append(imageB.astype(np.float32)/255.0)
+            batchA.append(imageA)
+            batchB.append(imageB)
+
             # batchA.append(imageA.flatten().astype(np.float32) / 255.0)
             # batchB.append(imageB.flatten().astype(np.float32) / 255.0)
 
