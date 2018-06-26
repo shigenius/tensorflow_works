252312e4ddf880017de23444482c1085c7138368
shigenius <k178578@ie.u-ryukyu.ac.jp>
2018-06-26 19:46:06 +0900

    modify tf-slim/transl

---
+++ tf-slim/script/makeConfusionMatrix.py
@@ -0,0 +1,56 @@
+import csv
+import argparse
+
+import numpy as np
+import seaborn as sn
+import matplotlib.pyplot as plt
+import re
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('csv_path', help='File name of csv')
+    parser.add_argument('label_path', help='File name of label.txt')
+    # parser.add_argument('num_class', help='num of class',
+    #                     default=6)
+    args = parser.parse_args()
+
+    label_file = open(args.label_path, "r", encoding="utf-8", errors="", newline="")
+
+    num_class = max([int(row.split(' ')[0]) for row in label_file])
+    num_class += 1
+    print("num_class:", num_class)
+
+    label_file = open(args.label_path, "r", encoding="utf-8", errors="", newline="")
+    labels = [row.split(' ')[1].strip() for row in label_file]
+    print(labels)
+
+
+    csv_file = open(args.csv_path, "r", encoding="utf-8", errors="", newline="")
+    f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"',
+                   skipinitialspace=True)
+    header = next(f)
+
+    confusion_mat = [[0 for i in range(num_class)] for j in range(num_class)]
+    for row in f:
+        esti = int(row[2])
+        gt = int(row[3])
+        confusion_mat[gt][esti] += 1
+
+    print("confision_mat:", confusion_mat)
+
+    estimation_acc = [l[i]/sum(l) for i, l in enumerate(confusion_mat)]
+    print("estimation acc:", estimation_acc)
+
+    mean_acc = sum([l[i] for i, l in enumerate(confusion_mat)]) / sum([sum(l) for i, l in enumerate(confusion_mat)])
+    print("mean acc:", mean_acc)
+
+    confusion_mat = [[n / sum(l) for n in l] for l in confusion_mat]
+    ndarr = np.array(confusion_mat)
+    print(ndarr)
+
+    plt.figure(figsize=(10, 7))
+    sn.heatmap(confusion_mat, annot=True, fmt='g', cmap='Blues', xticklabels=labels, yticklabels=labels)
+    plt.title("Confusion Matrix")
+    plt.xlabel("Predicted class")
+    plt.ylabel("True class")
+    plt.show()
