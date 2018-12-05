112e7affb403b645402b777389155951ac99c0a8
shigenius <k178578@ie.u-ryukyu.ac.jp>
2018-12-06 00:20:20 +0900

    modified:   crop_VOC.py

--- tf-slim/script/crop_VOC.py
+++ tf-slim/script/crop_VOC.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('voc_dir_path', type=str)
     parser.add_argument('output_dir_path', type=str)
-    parser.add_argument('class_name', type=str)
+    # parser.add_argument('class_name', type=str)
     args = parser.parse_args()
 
     yyyy = [i.name for i in os.scandir(path=args.voc_dir_path) if i.is_dir()]
@@ -55,43 +55,46 @@ if __name__ == '__main__':
 
     print(xmls)
 
-    target_infos = [get_anno_info(xml, args.class_name) for xml in xmls if is_target_class(xml, args.class_name)]
-    print(target_infos)
+    class_list = ['aeroplane','bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable',
+                  'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
+    for class_name in class_list:
+        target_infos = [get_anno_info(xml, args.class_name) for xml in xmls if is_target_class(xml, args.class_name)]
+        print(target_infos)
 
-    # prepare output dir
-    org_save_dir = os.path.join(args.output_dir_path, args.class_name)
-    crp_save_dir = os.path.join(args.output_dir_path, args.class_name+'_cropped')
-    os.makedirs(org_save_dir, exist_ok=True)
-    os.makedirs(crp_save_dir, exist_ok=True)
+        # prepare output dir
+        org_save_dir = os.path.join(args.output_dir_path, args.class_name)
+        crp_save_dir = os.path.join(args.output_dir_path, args.class_name+'_cropped')
+        os.makedirs(org_save_dir, exist_ok=True)
+        os.makedirs(crp_save_dir, exist_ok=True)
 
-    # prepare logs
-    log = open(os.path.join(crp_save_dir, "subwindow_log.txt"), 'w')
-    writer = csv.writer(log, lineterminator='\n')
-    writer.writerow(("id", "center_x", "center_y", "size_x", "size_y"))  # write header
+        # prepare logs
+        log = open(os.path.join(crp_save_dir, "subwindow_log.txt"), 'w')
+        writer = csv.writer(log, lineterminator='\n')
+        writer.writerow(("id", "center_x", "center_y", "size_x", "size_y"))  # write header
 
-    count = 1
-    # crop boundary box
-    for i in target_infos:
-        image_path = os.path.join(os.path.join(os.path.split(os.path.split(i['xmlpath'])[0])[0], 'JPEGImages'), i['filename']) # もうちょっといい書き方を
-        print(image_path)
-        assert os.path.exists(image_path), "file:{0}".format(f.split(" ")[0])
-        image = cv2.imread(image_path)
-        # print(image.shape) # (h, w d)
+        count = 1
+        # crop boundary box
+        for i in target_infos:
+            image_path = os.path.join(os.path.join(os.path.split(os.path.split(i['xmlpath'])[0])[0], 'JPEGImages'), i['filename']) # もうちょっといい書き方を
+            print(image_path)
+            assert os.path.exists(image_path), "file:{0}".format(f.split(" ")[0])
+            image = cv2.imread(image_path)
+            # print(image.shape) # (h, w d)
 
-        bbs = i['bbs']
-        for bb in bbs:
-            idx = '%04d' % count
-            crop = image[bb['ymin']:bb['ymax'], bb['xmin']:bb['xmax'], :]
-            shutil.copyfile(image_path, os.path.join(org_save_dir, 'image_'+idx+'.jpg'))
-            cv2.imwrite(os.path.join(crp_save_dir, 'image_'+idx+'.jpg'), crop)
+            bbs = i['bbs']
+            for bb in bbs:
+                idx = '%04d' % count
+                crop = image[bb['ymin']:bb['ymax'], bb['xmin']:bb['xmax'], :]
+                shutil.copyfile(image_path, os.path.join(org_save_dir, 'image_'+idx+'.jpg'))
+                cv2.imwrite(os.path.join(crp_save_dir, 'image_'+idx+'.jpg'), crop)
 
-            w = bb['xmax'] - bb['xmin']
-            h = bb['ymax'] - bb['ymin']
-            writer.writerow((count, bb['xmin'] + w/2, bb['ymin'] + h/2, w, h))
+                w = bb['xmax'] - bb['xmin']
+                h = bb['ymax'] - bb['ymin']
+                writer.writerow((count, bb['xmin'] + w/2, bb['ymin'] + h/2, w, h))
+    
+                count += 1
 
-            count += 1
 
 
-
-            # cv2.imshow("hoge", crop)
-            # cv2.waitKey(0)
+                # cv2.imshow("hoge", crop)
+                # cv2.waitKey(0)
